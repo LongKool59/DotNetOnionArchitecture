93a5b79f9efedc9b87f331dd19d1476e7b0612fd
Long Nguyen <nguyenkimlog59@gmail.com>
2022-06-13 15:47:15 +0700

    Generate database

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+        }
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,4 +1,7 @@
 using Application;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,21 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+#region API Versioning
+// Add API Versioning to the Project
+builder.Services.AddApiVersioning(config =>
+{
+    // Specify the default API Version as 1.0
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    // If the client hasn't specified the API version in the request, use the default API version number 
+    config.AssumeDefaultVersionWhenUnspecified = true;
+    // Advertise the API versions supported for the particular endpoint
+    config.ReportApiVersions = true;
+});
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
